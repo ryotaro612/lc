6f9ea05752d7b7afadb37312234cedd36f49ba94
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-29 19:42:03 +0900

    unique_email_addresses

--- google/cc/ip/unique_email_addresses.cc
+++ google/cc/ip/unique_email_addresses.cc
@@ -1,7 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main() {
-	cout << "doge";
-}
+class Solution {
+  public:
+    int numUniqueEmails(vector<string> &emails) {
+        set<string> normalized_mails;
+        for(auto email : emails)
+            normalized_mails.insert(normalize(email));
+
+        return normalized_mails.size();
+    }
+
+    string normalize(string email) {
+        string local = "";
+        auto at = email.find('@');
+        for(int i = 0; i < at; i++) {
+            char c = email[i];
+            if(c == '.')
+                continue;
+            if(c == '+')
+                break;
+            local.push_back(c);
+        }
+        auto domain = string(email.begin() + at, email.end());
+        return local + domain;
+    }
+};
