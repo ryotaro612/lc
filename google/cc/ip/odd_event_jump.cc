0f68e32751de42507e9293a0bdbd4735a6698492
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-01 17:06:38 +0900

    odd_event_jump

--- google/cc/ip/odd_event_jump.cc
+++ google/cc/ip/odd_event_jump.cc
@@ -7,34 +7,29 @@ class Solution {
     int oddEvenJumps(vector<int> &arr) {
         int n = arr.size();
         vector<vector<bool>> dp(n, vector<bool>(2, false));
+        vector<int> idx(100000, 0);
         dp[n - 1][0] = dp[n - 1][1] = true;
-        vector<int> sorted;
-        map<int, int> index;
-        sorted.push_back(arr[n - 1]);
-        index[arr[n - 1]] = n - 1;
+        set<int> sorted;
+        sorted.insert(arr[n - 1]);
+        idx[arr[n - 1]] = n - 1;
         for(int i = n - 2; i >= 0; i--) {
-            auto iter = lower_bound(sorted.begin(), sorted.end(), arr[i]);
-            //cout << "->" << i << endl;
+            auto iter = sorted.lower_bound(arr[i]);
 
             if(iter != sorted.end()) {
-                dp[i][0] = dp[index[*iter]][1];
+                dp[i][0] = dp[idx[*iter]][1];
                 if(arr[i] == *iter) {
-                    dp[i][1] = dp[index[*iter]][0];
+                    dp[i][1] = dp[idx[*iter]][0];
                 } else if(iter != sorted.begin()) {
-                    dp[i][1] = dp[index[*(iter - 1)]][0];
+                    dp[i][1] = dp[idx[*(--iter)]][0];
+                    iter++;
                 }
             } else {
-                dp[i][1] = dp[index[*(iter - 1)]][0];
+                dp[i][1] = dp[idx[*(--iter)]][0];
+                iter++;
             }
-            sorted.push_back(arr[i]);
-            sort(sorted.begin(), sorted.end());
-            index[arr[i]] = i;
+            sorted.insert(arr[i]);
+            idx[arr[i]] = i;
         }
-        /*
-        for(int i = 0; i < n; i++) {
-            cout << i << " (" << dp[i][0] << ", " << dp[i][1] << ")" << endl;
-        }
-        */
 
         int ans = 0;
         for(int i = 0; i < (int)arr.size(); i++)
@@ -43,12 +38,3 @@ class Solution {
         return ans;
     }
 };
-/*
-int main() {
-    Solution s;
-    vector<int> arr = {10, 13, 12, 14, 15};
-    auto ans = s.oddEvenJumps(arr);
-    cout << ans << endl;
-    return 0;
-}
-*/
