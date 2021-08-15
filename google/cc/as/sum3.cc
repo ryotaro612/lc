d46030955108447ada47c6d31ad3f21c9d2999f2
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-08-15 10:33:10 +0900

    sum3

--- google/cc/as/sum3.cc
+++ google/cc/as/sum3.cc
@@ -9,22 +9,24 @@ class Solution {
         if(n < 3)
             return {};
         vector<set<int>> mp(200001);
+        vector<int> nm;
         for(int i = 0; i < n; i++) {
             set<int> *p = &mp[nums[i] + 100000];
             if((*p).size() < 3) {
-                (*p).insert(i);
+                (*p).insert(nm.size());
+                nm.push_back(nums[i]);
             }
         }
         set<vector<int>> ans;
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int v = -nums[i] - nums[j];
+        for(int i = 0; i < nm.size() - 1; i++) {
+            for(int j = i + 1; j < nm.size(); j++) {
+                int v = -nm[i] - nm[j];
                 if(v + 100000 < 0 || v + 100000 > 200000)
                     continue;
 
                 auto found = mp[100000 + v];
                 if(found.upper_bound(j) != found.end()) {
-                    vector<int> a = {nums[i], nums[j], -(nums[i] + nums[j])};
+                    vector<int> a = {nm[i], nm[j], -(nm[i] + nm[j])};
                     sort(a.begin(), a.end());
                     ans.insert(a);
                 }
