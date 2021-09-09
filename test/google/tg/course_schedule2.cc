ca0a1859b96828e73f3aa3aef2462d250b27c2a9
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-09-09 19:22:01 +0900

    run valid tests

--- test/google/tg/course_schedule2.cc
+++ test/google/tg/course_schedule2.cc
@@ -1,27 +1,27 @@
 #include "google/tg/course_schedule2.cc"
 #include <gtest/gtest.h>
 
-// TEST(course_schedule2, 1) {
+TEST(course_schedule2, 1) {
 
-//     Solution s;
-//     vector<vector<int>> prerequistes = {{1, 0}};
-//     vector<int> res = s.findOrder(2, prerequistes);
+    Solution s;
+    vector<vector<int>> prerequistes = {{1, 0}};
+    vector<int> res = s.findOrder(2, prerequistes);
 
-//     vector<vector<int>> answers = {{0, 1}};
-//     EXPECT_TRUE(find(answers.begin(), answers.end(), res) != answers.end());
+    vector<vector<int>> answers = {{0, 1}};
+    EXPECT_TRUE(find(answers.begin(), answers.end(), res) != answers.end());
 
-// }
+}
 
-// TEST(course_schedule2, 2) {
+TEST(course_schedule2, 2) {
 
-//     Solution s;
-//     vector<vector<int>> prerequistes = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
-//     vector<int> res = s.findOrder(4, prerequistes);
+    Solution s;
+    vector<vector<int>> prerequistes = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+    vector<int> res = s.findOrder(4, prerequistes);
 
-//     vector<vector<int>> answers = {{0, 2, 1, 3}, {0, 1, 2, 3}};
+    vector<vector<int>> answers = {{0, 2, 1, 3}, {0, 1, 2, 3}};
 
-//     EXPECT_TRUE(find(answers.begin(), answers.end(), res) != answers.end());
-// }
+    EXPECT_TRUE(find(answers.begin(), answers.end(), res) != answers.end());
+}
 
 TEST(course_schedule2, 3) {
 
