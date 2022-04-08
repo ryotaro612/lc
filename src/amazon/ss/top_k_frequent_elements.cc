9f21e43dabd5a397d7044fbf8ebc6ef391d070f6
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-04-08 11:43:42 +0900

    quickselect.

--- src/amazon/ss/top_k_frequent_elements.cc
+++ src/amazon/ss/top_k_frequent_elements.cc
@@ -8,23 +8,52 @@ public:
     for (int num : nums) {
       counter[num]++;
     }
-    multiset<pair<int, int>> num_set;
-    for (pair<int, int> num_count : counter) {
-      int num = num_count.first;
-      int count = num_count.second;
-      if (static_cast<int>(num_set.size()) < k)
-        num_set.insert({count, num});
-      else {
-        if ((*num_set.begin()).first < count) {
-          num_set.erase(num_set.begin());
-          num_set.insert({count, num});
+    int n = counter.size();
+    vector<pair<int, int>> num_count;
+    for (pair<int, int> e : counter) {
+      num_count.push_back(e);
+    }
+    /*
+    for(auto e: num_count) {
+        cout << "(" << e.first <<"," << e.second << ") ";
+    }
+    */
+    // cout << endl;
+    int left = 0, right = n - 1, pivot = num_count[0].second;
+    while (true) {
+      for (; left < right;) {
+        if (num_count[left].second < pivot) {
+          swap(num_count[left], num_count[right]);
+          right--;
+        } else if (pivot <= num_count[right].second) {
+          swap(num_count[left], num_count[right]);
+          left++;
+        } else {
+          left++;
+          right--;
         }
       }
+      /*
+      cout << "pivot "<< pivot << ", left " << left << ", right " << right <<
+      endl; for(auto e: num_count) { cout << "(" << e.first <<"," << e.second <<
+      ") ";
+      }
+      cout << endl;
+      */
+      if (left + 1 < k) { // pivotが多きすぎる
+        pivot = num_count[left].second;
+        left = right;
+        right = n - 1;
+      } else if (left + 1 == k) { // k <= left
+        break;
+      } else { // k < left + 1 pivotが小さすぎる
+        pivot = num_count[right].second;
+        left = 0;
+      }
     }
-    vector<int> result;
-    for (pair<int, int> count_num : num_set) {
-      result.push_back(count_num.second);
-    }
+    vector<int> result(k);
+    for (int i = 0; i < k; i++)
+      result[i] = num_count[i].first;
     return result;
   }
 };
