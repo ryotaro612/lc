4151e6e7920090bd055e900a9adaca8a17af24fa
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-05-12 13:02:15 +0900

    binary search tree iterator

--- src/facebook/design/binary_search_tree_iterator.py
+++ src/facebook/design/binary_search_tree_iterator.py
@@ -43,3 +43,31 @@ class BSTIterator:
 # obj = BSTIterator(root)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self._push_left(root)
+
+    def next(self) -> int:
+        node = self.stack.pop()
+        self._push_left(node.right)
+        return node.val 
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+    
+    def _push_left(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
+
+"""
