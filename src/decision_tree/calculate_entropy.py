d276e11990fef2eccd1473819a305ed01a3646a1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-10-31 09:05:27 -0400

    calculate entropy

---
+++ src/decision_tree/calculate_entropy.py
@@ -0,0 +1,13 @@
+from collections import Counter
+import math
+
+class Solution:
+    def calculateEntropy(self, input: List[int]) -> float:
+        counter = Counter(input)
+        
+        n = len(input)
+        result = 0
+        
+        for v in counter.values():
+            result -= v / n * math.log2(v/n)
+        return result
