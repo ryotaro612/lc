fb392aac278a1f17c61ebca6f84541d59f3ccfe8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-11-23 08:21:49 -0500

    check if n and its double exist

---
+++ src/array101/check_if_n_and_its_double_exist.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        n = len(arr)
+        
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if arr[i] == 2 * arr[j] or arr[i] * 2 == arr[j]:
+                    return True
+        
+        return False
