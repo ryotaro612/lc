8939abacf55733c575bfbd530249fd319942c754
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-12-01 08:34:26 -0500

    string transforms into another string

---
+++ src/lc_curated_algo170/string_transforms_into_another_string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConvert(self, str1: str, str2: str) -> bool:
+        if str1 == str2:
+            return True
+        conv = dict()
+        n = len(str1)
+        
+        for c1, c2 in zip(str1, str2):
+            if c1 in conv:
+                if conv[c1] != c2:
+                    return False
+            else:
+                conv[c1] = c2
+                
+        if len(set(str2)) == 26:
+            return False
+        return True
