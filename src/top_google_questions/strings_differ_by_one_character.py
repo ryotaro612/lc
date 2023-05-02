cb3eb27159c67eaeeb750913bd73c4d98b65d3f1
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-05-02 12:10:55 -0400

    rabin carp

--- src/top_google_questions/strings_differ_by_one_character.py
+++ src/top_google_questions/strings_differ_by_one_character.py
@@ -1,3 +1,4 @@
+"""    
 class Solution:
     def differByOne(self, dicti: List[str]) -> bool:
         # l
@@ -11,3 +12,45 @@ class Solution:
                     return True
                 subwords.add(sub)
         return False
+"""
+
+
+class Solution:
+    def differByOne(self, dicti: List[str]) -> bool:
+        mod = 1000000007
+        h = 1 << 64
+
+        hv = []
+        for word in dicti:
+            v = 0
+            for c in word:
+                v *= mod
+                v %= h
+                v += ord(c) - ord("a")
+                v %= h
+            hv.append(v)
+
+        l = len(dicti[0])
+        for i in range(l):
+            found = set()
+
+            for j, word in enumerate(dicti):
+                c = ord(word[i]) - ord("a")
+                sub = hv[j] - c * self.modpow(mod, (l - 1 - i), h) % h
+                if sub < 0:
+                    sub += h
+                if sub in found:
+                    return True
+                found.add(sub)
+        return False
+
+    def modpow(self, x, n, mod):
+        res = 1
+        while n:
+            if n & 1:
+                res *= x
+                res %= mod
+            x *= x
+            x %= mod
+            n >>= 1
+        return res
