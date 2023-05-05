c4c096d08486f1b6f11cdc8488e3729980889a02
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-05-05 10:24:46 -0400

    change meidum format to hh:mm:dd

--- median.py
+++ median.py
@@ -2,21 +2,28 @@
 import csv
 
 
-def time_to_min(time):
+def time_to_sec(time):
     """Format HH:mm:ss to minutes."""
     h, m, s = time.split(":")
-    return (int(h) * 3600 + int(m) * 60 + int(s)) / 60
+    return int(h) * 3600 + int(m) * 60 + int(s)
 
 
 def compute_median(difficulty):
     """Compute the median of time spent on a problem."""
-    with open('./sessions.csv') as f:
+    with open("./sessions.csv") as f:
         reader = csv.DictReader(f)
         times = sorted(
-            [time_to_min(r["Time"]) for r in reader if r["Difficulty"] == difficulty]
+            [time_to_sec(r["Time"]) for r in reader if r["Difficulty"] == difficulty]
         )
         if len(times) % 2:
             median = times[len(times) // 2]
         else:
             median = (times[len(times) // 2 - 1] + times[len(times) // 2]) / 2
-        return [["Median(Elapsed time)"], None, [int(median)]]
+        hours = int(median // 3600)
+        miniutes = int(median % 3600 // 60)
+        seconds = int(median % 60)
+        return [
+            ["Median(Elapsed time)"],
+            None,
+            [f"{'%02d' % hours}:{'%02d' % miniutes}:{'%02d' % seconds}"],
+        ]
