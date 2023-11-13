ef4050dc3e5cb115cd974bc0a807b2000687dd9f
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-11-13 09:39:48 -0500

    a

--- src/serial/sort_vowels_in_a_string.py
+++ src/serial/sort_vowels_in_a_string.py
@@ -1,18 +1,23 @@
-lass Solution:
+
+from collections import defaultdict
+
+class Solution:
     def sortVowels(self, s: str) -> str:
         letters = [c for c in s]
-        vowels = []
+        vowels = defaultdict(int)
         vowel_set = {c for c in "AEIOUaeiou"}
         n = len(letters)
         for i in range(n):
             if letters[i] in vowel_set:
-                vowels.append(letters[i])
+                vowels[letters[i]] += 1
         
-        vowels.sort()
+        vowel_list = sorted([[letter,freq] for letter, freq in vowels.items()])
         v_i = 0
         for i in range(n):
             if letters[i] in vowel_set:
-                letters[i] = vowels[v_i]
-                v_i += 1
+                letters[i] = vowel_list[v_i][0]
+                vowel_list[v_i][1] -= 1
+                if not vowel_list[v_i][1]:
+                    v_i += 1
         
         return ''.join(letters)
