3d5b4db106f203306e2c8ea35acb2e8a99038186
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2024-03-12 10:48:48 +0900

    linked list

--- src/serial/remove_zero_sum_consecutive_nodes_from_linked_list.py
+++ src/serial/remove_zero_sum_consecutive_nodes_from_linked_list.py
@@ -5,41 +5,23 @@
 #         self.next = next
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        while True:
-            head, found = self.remove_consec(head)
-            if not found:
-                return head
+        node = sentinel = ListNode(0, head)
+        prefix_sum = dict()
+        cur_sum = 0
         
-    def remove_consec(self, head):
-        """
-        prefix_sum_d = dict()
-        pref
-        """
-        node = head
-        nodes = []
         while node:
-            nodes.append(node)
+            cur_sum += node.val
+            if cur_sum not in prefix_sum:
+                prefix_sum[cur_sum] = node
+            else:
+                remove_node = prefix_sum[cur_sum].next
+                remove_sum = cur_sum + remove_node.val
+                prefix_sum[cur_sum].next = node.next
+                while remove_node != node:
+                    del prefix_sum[remove_sum]
+                    remove_sum += remove_node.next.val
+                    remove_node = remove_node.next
+                
             node = node.next
-        
-        if [node for node in nodes if node.val == 0]:
-            return self.build_list([node for node in nodes if node.val]), True
-        
-        prefix_sum = {0: -1}
-        current_sum = 0
-        for i, node in enumerate(nodes):
-            current_sum += node.val
-            if current_sum in prefix_sum:
-                result = nodes[:prefix_sum[current_sum]+1] + nodes[i+1:]
-                return self.build_list(result), True
-            prefix_sum[current_sum] = i
-        return head, False
-
-    def build_list(self, nodes):
-        sentinel = ListNode()
-        cur = sentinel
-        for node in nodes:
-            cur.next = node
-            cur = node
-        cur.next = None
+                
         return sentinel.next
