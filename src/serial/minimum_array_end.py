d97e2d36b7e7f51dfe5b430b23c1ad2bd1d58690
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-04-01 12:05:03 +0900

    3133

---
+++ src/serial/minimum_array_end.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        """
+        x = 10101
+             * *
+             2^2
+             nums[n-1]
+             argmin x, n <= x^2
+        """
+        result = 0
+        j = 0
+
+        for i in range(64):    
+            if x & (1<<i):
+                result |= (1 << i)
+            else:
+                if (n-1)  & (1<< j):
+                    result |= (1<<i)
+                j += 1
+        return result
+        
