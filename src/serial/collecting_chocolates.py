d5d81aa8e706be29e2ffbbc591c528f9d4bd348a
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-05-04 12:26:06 +0900

    a

---
+++ src/serial/collecting_chocolates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCost(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        costs = [float('inf')] * n
+        types = [i for i in range(n)]
+        result = float('inf')
+        for r in range(n):
+            for i in range(n):
+                costs[types[i]] = min(costs[types[i]], nums[i])
+
+            result = min(result, r * x + sum(costs))
+            
+            next_types = [0] * n
+            for i in range(n):
+                next_types[i] = types[(i+1)%n]
+            types = next_types
+
+        
+        return result
