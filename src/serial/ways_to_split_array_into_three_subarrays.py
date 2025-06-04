c0b5b1fc95fe233ee827b6b995ac57e059fe0d49
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-06-04 13:41:42 +0900

    1712

--- src/serial/ways_to_split_array_into_three_subarrays.py
+++ src/serial/ways_to_split_array_into_three_subarrays.py
@@ -1,43 +1,27 @@
 class Solution:
     def waysToSplit(self, nums: List[int]) -> int:
-        result = 0
-        mod = 10**9 + 7
         n = len(nums)
-
         prefix = [0] * (n+1)
-        for i in range(n):
-            prefix[i+1] = prefix[i] + nums[i]
+        for i, num in enumerate(nums):
+            prefix[i+1] = prefix[i] + num
         
-        for i in range(1, n):
-            lb = i
-            ub = n
-            while ub - lb > 1:
-                mid = (lb + ub) // 2
-                # [:i] [i:mid]
-                if prefix[i] > prefix[mid] - prefix[i]:
-                    lb = mid
-                else:
-                    ub = mid
-            if ub == n:
-                continue
-            if not (prefix[i] <= prefix[ub] - prefix[i] <= prefix[n] - prefix[ub]):
-                continue
-            left = ub
-            lb = left - 1
-            ub = n
-            while ub - lb > 1:
-                mid = (ub + lb) // 2
-                if prefix[mid] - prefix[i] > prefix[n] - prefix[mid]:
-                    ub = mid
-                    # print('ub', mid)
-                else:
-                    lb = mid
-                    # print('lb', mid)
-            if lb == n:
-                continue
-            # print(i, left, lb)
-            if prefix[lb] - prefix[left] <= prefix[n] - prefix[lb]:
-                result += lb - left + 1
+        j = 2
+        k = j
+        result = 0
+        mod = 10**9+7
+        # 0 1 2
+        for i in range(1, n-1):
+            j = max(i+1, j)
+            while j < n-1 and prefix[i] > prefix[j] - prefix[i]:
+                j += 1
+            
+            k = max(j, k)
+            while k < n - 1 and prefix[n] - prefix[k+1] >= prefix[k+1] - prefix[i]:
+                k += 1
+            
+            # print(i, j, k)
+            if prefix[i] <= prefix[k] - prefix[i] <= prefix[n] - prefix[k]:
+                result += k - j + 1
                 result %= mod
-
-        return result                
+        
+        return result
