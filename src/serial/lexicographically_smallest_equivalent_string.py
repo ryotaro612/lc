dd2a44f2a843d39deb4b494438a89af3df143ea6
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-06-05 12:17:44 +0900

    cmoid

--- src/serial/lexicographically_smallest_equivalent_string.py
+++ src/serial/lexicographically_smallest_equivalent_string.py
@@ -1,28 +1,25 @@
+from collections import defaultdict
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        
         par = [-1] * 26
-
         n  = len(s1)
         for i in range(n):
             a = ord('a')
             self.unite(ord(s1[i]) - a, ord(s2[i]) -a, par)
         
-        convert = dict()
+        groups = [[] for _ in range(26)]
         for i in range(26):
-            letter = chr(i + ord('a'))
-            convert[letter] = letter
-            for j in range(i):
-                if self.is_same(i, j, par):
-                    convert[letter] = chr(j + ord('a'))
-                    break
-            
-        result = []
-        for c in baseStr:
-            result.append(convert[c])
-        
-        return ''.join(result)
+            root = self.find_root(i, par)
+            groups[root].append(i)
+        convert = dict()
 
+        for group in [group for group in groups if group]:
+            mini = min(group)
+            a = ord('a')
+            for i in group:
+                convert[chr(a + i)] = chr(a + mini)
+
+        return ''.join([convert[c] for  c in baseStr])
 
     def find_root(self, i, par):
         if par[i] < 0:
