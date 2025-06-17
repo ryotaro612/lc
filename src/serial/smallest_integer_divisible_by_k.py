364ed32d858f26737de7448e1da71f61899e2580
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-06-17 11:45:41 +0900

    1015

--- src/serial/smallest_integer_divisible_by_k.py
+++ src/serial/smallest_integer_divisible_by_k.py
@@ -1,19 +1,11 @@
+           
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
-        """
-        # 1 % k
-        mod = 1 % k
-        mod * 10 + 1 % 
-        """
         found = set()
-        val = 1
-        while True:
-            if val % k == 0:
-                return len(found) + 1
-            
-            if val % k in found:
-                return -1
-            
-            found.add(val % k)
-            val = val % k * 10 + 1
-            
+        val = 1 % k
+        while val not in found and val:
+            found.add(val)
+            val = (val * 10 + 1) % k
+        
+        return -1 if val else len(found) + 1
+       
