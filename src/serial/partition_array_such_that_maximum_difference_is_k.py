db146b617e3ce229a95456f502f54f15785bc526
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-06-19 11:37:16 +0900

    2294

--- src/serial/partition_array_such_that_maximum_difference_is_k.py
+++ src/serial/partition_array_such_that_maximum_difference_is_k.py
@@ -1,30 +1,11 @@
+import math
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
-        
         nums.sort()
-
-        partitions = self.divide(nums, k)
-        rev_partitions = self.divide(nums[::-1], k)
-
-        return min(len(partitions), len(rev_partitions))
-
-    
-    def divide(self, nums, k):
-
-        result = []
-
-        n = len(nums)
-        cur = []
-        for num in nums:
-            if cur:
-                if abs(cur[0] - num) <= k:
-                    cur.append(num)
-                    continue
-                result.append(cur)
-                cur = [num]
-                continue
-
-            cur = [num]
-        
-        result.append(cur)
-        return result
+        result = 0
+        mini = nums[0]
+        for num in nums[1:]:
+            if num - mini > k:
+                result += 1
+                mini = num
+        return result + 1
