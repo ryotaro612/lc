4b22632662f0772509ac14b74aedbe176b41116d
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-07-19 13:10:56 +0900

    a

--- src/serial/remove_sub_folders_from_the_filesystem.py
+++ src/serial/remove_sub_folders_from_the_filesystem.py
@@ -28,3 +28,41 @@ class Solution:
                         node = node[directory]
             
         return result
+"""
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        """
+        """
+        result = []
+        folder.sort()
+        trie = [dict(), False]
+
+        for path in folder:
+            nodes = [node for node in path.split('/') if node]
+
+            cur = trie
+            ok = True
+            for node in nodes:
+                if node in cur[0]:
+                    cur = cur[0][node]
+                    if cur[1]:
+                        ok = False
+                        break
+                else:
+                    break
+            if ok:
+                result.append(path)
+
+            cur = trie
+            for node in nodes:
+                if node in cur[0]:
+                    cur = cur[0][node]
+                else:
+                    cur[0][node] = [dict(), False]
+                    cur = cur[0][node]
+            cur[1] = True
+
+        return result
+
+
+"""
