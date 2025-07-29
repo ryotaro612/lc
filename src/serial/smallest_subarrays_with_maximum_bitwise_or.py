8327537267d31df21c82a54c2f5480dac3da12f2
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-07-29 12:26:20 +0900

    rev

--- src/serial/smallest_subarrays_with_maximum_bitwise_or.py
+++ src/serial/smallest_subarrays_with_maximum_bitwise_or.py
@@ -1,41 +1,17 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        """
-        result = []
-        bit_index = []
-        bit_index[i]
-        bx10010
-            i
-        bit_index[0] contains 0
-        
-        nums[j]
-        result[j] = k - j + 1
-        """
-        bound = 32
-        bit_index = [[] for _ in range(bound)]
-        for i, num in enumerate(nums):
-            j = 0
-            while num:
-                if num & 1:
-                    bit_index[j].append(i)
-                num >>= 1
-                j += 1
-        
-        for i in range(bound):
-            bit_index[i] = bit_index[i][::-1]
-        
-        result = []
-        for i, num in enumerate(nums):
-            cand = []
-            for j in range(bound):
-                while bit_index[j] and bit_index[j][-1] < i:
-                    bit_index[j].pop()
-
-                if bit_index[j]:
-                    cand.append(bit_index[j][-1])
+        bits = [-1] * 31
+        n = len(nums)
+        result = [-1] * n
+        for i in range(n-1, -1, -1):
+            k = i
+            for j in range(31):
+                if nums[i] & (1<<j):
+                    bits[j] = i
+                else:
+                    if bits[j] >= 0:
+                        k = max(k, bits[j])
             
-            if cand:
-                result.append(max(cand) - i + 1)
-            else:
-                result.append(1)
+            result[i] = k - i + 1
+        
         return result
