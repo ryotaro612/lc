d095cd3c106dc2bf078a42c03e06b98c9798b111
Ryotaro <ryotaro.nakamura@uzabase.com>
2025-08-05 12:06:30 +0900

    fix

--- src/serial/count_special_triplet.py
+++ src/serial/count_special_triplet.py
@@ -1,34 +1,18 @@
 from collections import defaultdict
-import bisect
 
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        """
-        num * 2, i
-
-        v < i
-        v2 > i
-        v * v2
-        """
-        val_idx = defaultdict(list)
-
+        left = defaultdict(int)
+        right = defaultdict(int)
         for i, num in enumerate(nums):
-            val_idx[num].append(i)
-        
+            right[num] += 1
+
         result = 0
         mod = 10**9 + 7
-    
-        for j, num in enumerate(nums):
-            found = bisect.bisect_left(val_idx[num*2], j)
-            n = len(val_idx[num*2])
-
-            left = found
-            if found < n and val_idx[num*2][found] == j:
-                right = n - (found+1)
-            else:
-                right = n - found
-            
-            result += left * right
+        for num in nums:
+            right[num] -= 1
+            result += right[num*2] * left[num*2]
             result %= mod
+            left[num] += 1
         
         return result
