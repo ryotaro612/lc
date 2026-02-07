8395250fd3449ffb9835a5cea4f26e95d5daa155
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-02-07 12:06:14 +0900

    2645

--- src/2600/minimum_additions_to_make_valid_string.py
+++ src/2600/minimum_additions_to_make_valid_string.py
@@ -1,39 +1,9 @@
 class Solution:
     def addMinimum(self, word: str) -> int:
-        """
-        prev = 'a' or 'b' 'c'
-        result = 0
+        prev = 'z'
+        k = 0
         for c in word:
-
-        """
-        result = 0
-        prev = word[0]
-        if prev == 'b':
-            result += 1
-        elif prev == 'c':
-            result += 2
-        
-        for c in word[1:]:
-            if c == 'a':
-                if prev == 'a':
-                    result += 2
-                elif prev == 'b':
-                    result += 1
-            elif c == 'b':
-                if prev == 'b':
-                    result += 2
-                elif prev == 'c':
-                    result += 1
-            else: # c == 'c'
-                if prev == 'a':
-                    result += 1
-                elif prev == 'c':
-                    result += 2
+            k += c <= prev
             prev = c
-        
-        if prev == 'a':
-            result += 2
-        elif prev == 'b':
-            result += 1
-        
-        return result
+
+        return k * 3 - len(word)   
