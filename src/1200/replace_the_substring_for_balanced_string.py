ea4e6aae19b7ee41c1c3e19dc24a8e42422a6da7
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-02-10 12:32:12 +0900

    1234

--- src/1200/replace_the_substring_for_balanced_string.py
+++ src/1200/replace_the_substring_for_balanced_string.py
@@ -1,33 +1,13 @@
-import bisect
-
+from collections import Counter
 class Solution:
     def balancedString(self, s: str) -> int:
-        """
-        prefix[i] - prefix[j] sum of elements between i:j
-        """
-        n = len(s)
-        prefix = [[0] * (n+1) for _ in range(4)]
-        mp = {'Q': 0, 'W': 1, 'E': 2, 'R': 3}
-
-        for i, c in enumerate(s):
-            for j in range(4):
-                prefix[j][i+1] = prefix[j][i]
-            prefix[mp[c]][i+1] += 1
-        
-        result = n
-        for i, c in enumerate(s):
-            ub = n + 1
-            lb = i - 1
-            while ub - lb > 1:
-                mid = (ub + lb) // 2
-                ok = True
-                for j in range(4):
-                    if prefix[j][n] - (prefix[j][mid] - prefix[j][i]) > n // 4:
-                        ok = False
-                if ok:
-                    ub = mid
-                    result = min(result, ub - i)
-                else:
-                    lb = mid
-            
+        counter = Counter(s)
+        left = 0
+        result = n = len(s)
+        for right, c in enumerate(s):
+            counter[c] -= 1
+            while left < n and all(counter[c] <= n // 4 for c in 'QWER'):
+                result = min(result, right - left + 1)
+                counter[s[left]] += 1
+                left += 1
         return result
